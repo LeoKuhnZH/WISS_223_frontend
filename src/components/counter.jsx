bac0d60d3b8ef3b085af7c661ef34f1adacca527
Vinci4600 <vincent.diergardt@gmail.com>
2026-03-25 10:27:15 +0100

    Cookie Klicker

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
+import cookieImage from "./img/c.jpg";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <p>Aktueller Zähler: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Erhöhen</button>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+
+    return (
+        <div>
+            <p>Aktueller Zähler: {count}</p>
+            {/* Nur das Bild ist klickbar */}
+            <img
+                src={cookieImage}
+                alt="Cookie"
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+                width="100px"
+            />
+        </div>
+    );
 };
 
 export default Counter;
